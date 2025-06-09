37d913737e15f56edd59e40ad3f946979dc0aa79
Samir Ribeiro <samir.sjr77@gmail.com>
2025-06-09 08:29:30 -0300

    ExecMedia: One button and Volume text

--- lib/executaMidia/main.dart
+++ lib/executaMidia/main.dart
@@ -32,27 +32,37 @@ class _MyHomePageState extends State<MyHomePage> {
   final _audioPlayer = AudioPlayer();
   dynamic _source;
   var _firstPlay = true;
+  var _playing = false;
   var _volume = 0.5;
 
   _playAudio() async {
     _audioPlayer.setVolume(_volume);
-    if(_firstPlay){
+    setState(() {
+      _playing = true;
+    });
+
+    if (_firstPlay) {
       // _source = UrlSource("https://www.soundhelix.com/examples/mp3/SoundHelix-Song-8.mp3");
       _source = AssetSource("audios/musica.mp3");
       await _audioPlayer.play(_source);
       _firstPlay = false;
-    }
-    else {
+    } else {
       _audioPlayer.resume();
     }
   }
 
   _pauseAudio() async {
+    setState(() {
+      _playing = false;
+    });
     await _audioPlayer.pause();
   }
 
   _stopAudio() async {
     _firstPlay = true;
+    setState(() {
+      _playing = false;
+    });
     await _audioPlayer.stop();
   }
 
@@ -60,56 +70,58 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.deepPurple, //Theme.of(context).colorScheme.inversePrimary,
-        title: Text(
-            widget.title,
-          style: TextStyle(color: Colors.white),
-        ),
+        backgroundColor: Colors.deepPurple,
+        //Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title, style: TextStyle(color: Colors.white)),
       ),
       body: Container(
         padding: EdgeInsets.all(16),
         child: Column(
           children: [
+            Text(
+              "Volume"
+            ),
             Slider(
               min: 0,
-                max: 1,
-                divisions: 10,
-                value: _volume,
-                onChanged: (newVolume){
-                  setState(() {
-                    _volume = newVolume;
-                  });
-                  _audioPlayer.setVolume(newVolume);
-                }
+              max: 1,
+              divisions: 10,
+              value: _volume,
+              onChanged: (newVolume) {
+                setState(() {
+                  _volume = newVolume;
+                });
+                _audioPlayer.setVolume(newVolume);
+              },
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Padding(
-                    padding: EdgeInsets.all(8),
+                  padding: EdgeInsets.all(16),
                   child: GestureDetector(
-                    onTap: _playAudio,
-                    child: Image.asset("assets/images/executar.png"),
+                    onTap: () {
+                      if (_playing) {
+                        _pauseAudio();
+                      } else {
+                        _playAudio();
+                      }
+                    },
+                    child: _playing
+                        ? Image.asset("assets/images/pausar.png")
+                        : Image.asset("assets/images/executar.png"),
                   ),
                 ),
                 Padding(
-                  padding: EdgeInsets.all(8),
-                  child: GestureDetector(
-                    onTap: _pauseAudio,
-                    child: Image.asset("assets/images/pausar.png"),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(8),
+                  padding: EdgeInsets.all(16),
                   child: GestureDetector(
                     onTap: _stopAudio,
                     child: Image.asset("assets/images/parar.png"),
                   ),
                 ),
               ],
-            )
+            ),
           ],
-        )
+        ),
       ),
     );
   }
