35ae5b70b09b7a808b611f67c5d5d5b639165650
Samir Ribeiro <samir.sjr77@gmail.com>
2025-06-11 20:57:26 -0300

    FirebaseApp: Remove Firebase to add again later

---
+++ lib/firebase/main.dart
@@ -0,0 +1,50 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class AppFirebase extends StatelessWidget {
+  const AppFirebase({super.key});
+
+  final title = "Firebase";
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: title,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: MyHomePage(title: title),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    Firebase.initializeApp();
+    FirebaseFirestore.instance
+        .collection("users")
+        .doc("score")
+        .set({"Samir" : "250", "Vanessa": "590"});
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        title: Text(widget.title, style: TextStyle(color: Colors.white)),
+      ),
+      body: Container(),
+    );
+  }
+}
