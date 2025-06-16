1fc8f600cd7768e91caa54af05c692326a58cfde
Samir Ribeiro <samir.sjr77@gmail.com>
2025-06-15 21:35:09 -0300

    Medical Gemini: Finish app

---
+++ lib/medicalGemini/chat_config.dart
@@ -0,0 +1,16 @@
+class MedicalChatConfigs {
+  static const String geminiModel = "gemini-2.0-flash";
+
+  static const String messageHint = "Digite sua mensagem...";
+
+  static const String welcomeMessage = "Olá 👋 Estou aqui para ajudar com duvidas medicas. Como posso te ajudar hoje?";
+
+  static const List<String> suggestions = [
+    "O que fazer para melhorar dor de cabeça?",
+    "Quando devo me consultar com um médico?"
+  ];
+
+  static const String systemInstruction = "Você é um assistente médico profissional de saúde."
+      "Responda apenas a perguntas relacionadas à saúde e à medicina, de forma concisa e direta, sem muitas explicações."
+      "Se a pergunta não for relacionada à saúde ou medicina, informe educadamente ao usuário que você só pode responder a perguntas relacionadas à medicina.";
+}
--- lib/medicalGemini/main.dart
+++ lib/medicalGemini/main.dart
@@ -1,22 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ai_toolkit/flutter_ai_toolkit.dart';
+import 'package:google_generative_ai/google_generative_ai.dart';
+import 'package:sjr77_first_project/medicalGemini/chat_config.dart';
+import 'package:sjr77_first_project/medicalGemini/keys.dart';
 
 class MedicalChat extends StatelessWidget {
   const MedicalChat({super.key});
 
+  final appName = "Gemini médico";
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Gemini medico',
+      title: appName,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
       ),
-      home: const MyHomePage(),
+      home: MyHomePage(title: appName),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -26,6 +34,32 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        automaticallyImplyLeading: false,
+        title: Text(widget.title),
+        centerTitle: true,
+      ),
+      body: LlmChatView(
+          suggestions: MedicalChatConfigs.suggestions,
+          style: LlmChatViewStyle(
+            chatInputStyle: ChatInputStyle(
+              hintText: MedicalChatConfigs.messageHint,
+              decoration: const BoxDecoration().copyWith(
+                borderRadius: BorderRadius.circular(50),
+              ),
+            ),
+          ),
+          provider: GeminiProvider(
+            model: GenerativeModel(
+              model: MedicalChatConfigs.geminiModel,
+              apiKey: ApiKeys.gemini,
+              systemInstruction: Content.system(MedicalChatConfigs.systemInstruction),
+            ),
+          ),
+          welcomeMessage: MedicalChatConfigs.welcomeMessage
+      ),
+    );
   }
 }
