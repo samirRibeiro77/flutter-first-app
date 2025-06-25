13afb481e0cca372d0a94202b2184ee09ffa4fb7
Samir Ribeiro <samir.sjr77@gmail.com>
2025-06-25 17:48:24 -0300

    Maps
    Show RJ Sugar Loaf on the map

--- lib/maps/main.dart
+++ lib/maps/main.dart
@@ -1,4 +1,6 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class AppMaps extends StatelessWidget {
   const AppMaps({super.key});
@@ -11,7 +13,7 @@ class AppMaps extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: title,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.greenAccent),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
         scaffoldBackgroundColor: Colors.white,
       ),
       home: MyHomePage(title: title),
@@ -29,12 +31,54 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final _controller = Completer<GoogleMapController>();
+  bool _mountain = true;
+
+  static const CameraPosition _sugarLoaf = CameraPosition(
+    target: LatLng(-22.950684,-43.1610249),
+    zoom: 15.5,
+  );
+
+  static const CameraPosition _sugarLoafHike = CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(-22.9529811,-43.1626918),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
+
+  Future<void> _goToTheClimb() async {
+    final GoogleMapController controller = await _controller.future;
+    await controller.animateCamera(CameraUpdate.newCameraPosition(_sugarLoafHike));
+
+    setState(() {
+      _mountain = false;
+    });
+  }
+
+  Future<void> _goToTheMountain() async {
+    final GoogleMapController controller = await _controller.future;
+    await controller.animateCamera(CameraUpdate.newCameraPosition(_sugarLoaf));
+
+    setState(() {
+      _mountain = true;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(backgroundColor: Colors.white, title: Text(widget.title)),
-      body: Center()
+      appBar: AppBar(title: Text(widget.title)),
+      body: GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: _sugarLoaf,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: _mountain ? _goToTheClimb : _goToTheMountain,
+        label: Text(_mountain ? "To the hike!" : "To the mountain!"),
+        icon: Icon(_mountain ? Icons.hiking : Icons.map_outlined),
+      ),
     );
   }
 }
